e614fc747a83bc0067de4b90ac38f604a0c1f7ff
Michael <michaelbubel.1989@gmail.com>
2020-12-28 21:16:52 -0800

    Added views for Department, Role, and Employee

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log("Employee Tracker");
 let mysql = require("mysql");
 let inquirer = require("inquirer");
+let cTable = require("console.table");
 
 let connection = mysql.createConnection({
   host: "localhost",
@@ -18,24 +19,79 @@ let connection = mysql.createConnection({
 
 connection.connect(function (err) {
   if (err) throw err;
-  runSearch();
+  mainMenu();
 });
 
-function runSearch() {
-  inquirer.prompt({
-    name: "action",
-    type: "list",
-    message: "What would you like to do?",
-    choices: [
-      "Add a department",
-      "Add a role",
-      "Add an employee",
-      "View departments",
-      "View roles",
-      "View employees",
-      "Update employee role",
-      "Exit"
+let INITIAL_MENU_ITEMS = {
+  ADD_DEPARTMENT: "Add a deparment",
+  ADD_ROLE: "Add a role",
+  ADD_EMPLOYEE: "Add an employee",
+  VIEW_DEPARTMENTS: "View departments",
+  VIEW_ROLES: "View roles",
+  VIEW_EMPLOYEES: "View employees",
+  UPDATE_EMPLOYEE_ROLE: "Update employee role",
+  EXIT: "Exit",
+};
 
-    ],
+function viewDepartments() {
+  connection.query("SELECT name FROM department", function (err, res) {
+    if (err) throw err;
+    console.log(" ");
+    console.table(res);
   });
+}
+function viewRoles() {
+  connection.query(
+    "SELECT d.name AS department, r.title, r.salary FROM role r INNER JOIN department d ON r.department_id = d.id",
+    function (err, res) {
+      if (err) throw err;
+      console.log(" ");
+
+      console.table(res);
+    }
+  );
+}
+
+function viewEmployees() {
+  connection.query(
+    "SELECT e.first_name, e.last_name, d.name AS department, r.title, r.salary, m.first_name manager_first_name FROM employee e INNER JOIN role r ON e.role_id = r.id LEFT JOIN employee m on m.id = e.manager_id INNER JOIN department d ON r.department_id = d.id",
+    function (err, res) {
+      if (err) throw err;
+      console.log(" ");
+      console.table(res);
+    }
+  );
+}
+
+function mainMenu() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        INITIAL_MENU_ITEMS.ADD_DEPARTMENT,
+        INITIAL_MENU_ITEMS.ADD_ROLE,
+        INITIAL_MENU_ITEMS.ADD_EMPLOYEE,
+        INITIAL_MENU_ITEMS.VIEW_DEPARTMENTS,
+        INITIAL_MENU_ITEMS.VIEW_ROLES,
+        INITIAL_MENU_ITEMS.VIEW_EMPLOYEES,
+        INITIAL_MENU_ITEMS.UPDATE_EMPLOYEE_ROLE,
+        INITIAL_MENU_ITEMS.EXIT,
+      ],
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case INITIAL_MENU_ITEMS.VIEW_DEPARTMENTS:
+          viewDepartments();
+          break;
+        case INITIAL_MENU_ITEMS.VIEW_ROLES:
+          viewRoles();
+          break;
+        case INITIAL_MENU_ITEMS.VIEW_EMPLOYEES:
+          viewEmployees();
+          break;
+      }
+      mainMenu();
+    });
 }
